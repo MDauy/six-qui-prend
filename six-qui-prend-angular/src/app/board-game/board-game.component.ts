05f6e549a173909a71bfa96b87fa88632805260d
Marion Daussy <marion.daussy@gmail.com>
2023-06-25 18:46:14 +0200

    Angular : Initialisation du board de jeu
    Joueurs initialisés avec main de départ, table de jeu

--- six-qui-prend-angular/src/app/board-game/board-game.component.ts
+++ six-qui-prend-angular/src/app/board-game/board-game.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { Player } from 'src/models/player';
 import { Card } from 'src/models/card';
+import cardsData from '../../cards.json'
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -12,19 +13,51 @@ import { Card } from 'src/models/card';
   templateUrl: './board-game.component.html',
   styleUrls: ['./board-game.component.less'],
 })
-export class BoardGameComponent {
-  players: Player[]
-  remainingCards: Card[];
-  tableCards: Card[][];
+export class BoardGameComponent implements OnInit {
+  players: Player[] = [];
+  cardsData: any = cardsData;
+  remainingCards: Card[] = [];
+  tableCards: Card[][] = [];
 
-  constructor(){
-    this.players = [];
-    this.remainingCards = [];
+  ngOnInit(): void {
+    this.players = [{
+      hand: [],
+      name: 'player1',
+      score: 0
+    }, {
+      hand: [],
+      name: 'player2',
+      score: 0
+    },
+    {
+      hand: [],
+      name: 'player3',
+      score: 0
+    },
+    {
+      hand: [],
+      name: 'player4',
+      score: 0
+    }];
+    this.remainingCards = this.cardsData.cards.sort(() => (Math.random() > .5) ? 1 : -1);
+    this.distributeToPlayers();
     this.tableCards = [
-      [{value: 1, points:1}, {value: 2, points:1}, {value: 3, points:1}],
-      [{value: 56, points:1}, {value: 70, points:1}, {value: 80, points:1}],
-      [{value: 82, points:1}, ],
-      [{value: 15, points:1}, {value: 17, points:1}]
-    ]
+      [this.pickFirstCard()!], [this.pickFirstCard()!], [this.pickFirstCard()!], [this.pickFirstCard()!]
+    ];
+  }
+
+  private pickFirstCard(): Card | undefined {
+    return this.remainingCards.shift();
+  }
+
+  private distributeToPlayers(): void {
+    for (let index = 0; index < this.players.length; index++) {
+      this.players[index].hand.push(this.pickFirstCard()!);
+      if (index == 3) {
+        if (this.players[index].hand.length < 10) {
+          index = -1;
+        }
+      }
+    }
   }
 }
