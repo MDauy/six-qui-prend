06691b8c9b614e4b73bf3f0ef37328add412e04c
Marion Daussy <marion.daussy@gmail.com>
2023-06-25 14:54:15 +0200

    ajout six qui prend DOTNET

---
+++ six-qui-prend-dotnet/six-qui-prend/Models/Card.cs
@@ -0,0 +1,8 @@
+﻿namespace six_qui_prend.Models
+{
+    public class Card
+    {
+        public int Value { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ six-qui-prend-dotnet/six-qui-prend/Models/GameBoard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace six_qui_prend.Models
+{
+    internal class GameBoard
+    {
+        public Dictionary<int, int> CardsStock{ get; set; }
+
+        public GameBoard ()
+        {
+            
+        }
+    }
+}
